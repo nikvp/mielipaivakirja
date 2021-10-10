dbad9a5e172ef34e8607b1c59cb26e4529824cb9
Nomsku <albin.sihvola@metropolia.fi>
2021-10-10 16:45:34 +0300

    Arviointi xml muutoksia

--- app/src/main/java/com/example/mielipaivakirja/paiva_arviointi.java
+++ app/src/main/java/com/example/mielipaivakirja/paiva_arviointi.java
@@ -3,6 +3,7 @@ package com.example.mielipaivakirja;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.SeekBar;
 
 public class paiva_arviointi extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class paiva_arviointi extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_paiva_arviointi);
+        SeekBar seekBarArvio = (SeekBar) findViewById(R.id.seekBarAhdistunut);
+        seekBarArvio.setMax(100);
     }
+
 }
