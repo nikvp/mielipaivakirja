dcc871b364242a215eea133cc3e31a37d6ff0f12
jjaadi <Jan.Blatter@metropolia.fi>
2021-10-11 16:58:30 +0300

    saavutukset, tallennus stringinä

--- app/src/main/java/com/example/mielipaivakirja/paiva_saavutukset.java
+++ app/src/main/java/com/example/mielipaivakirja/paiva_saavutukset.java
@@ -3,12 +3,28 @@ package com.example.mielipaivakirja;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class paiva_saavutukset extends AppCompatActivity {
+
+    TextView textView;
+    EditText inputText;
+    String saavutukset;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_paiva_saavutukset);
+
+        inputText = (EditText) findViewById(R.id.inputSaavutukset);
+    }
+
+    public void saveText(View view) {
+        saavutukset = String.valueOf(inputText.getText());
+        System.out.println(saavutukset);
     }
 }
