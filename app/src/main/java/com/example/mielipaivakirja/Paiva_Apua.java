7d1bf24f9f9c9b6a64b8f8b5a861bcba6fb3d6b2
Nomsku <albin.sihvola@metropolia.fi>
2021-10-12 13:32:28 +0300

    tarkastelu xml muutoksia ja väriteema muutoksia

---
+++ app/src/main/java/com/example/mielipaivakirja/Paiva_Apua.java
@@ -0,0 +1,14 @@
+package com.example.mielipaivakirja;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Paiva_Apua extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_paiva_apua);
+    }
+}
